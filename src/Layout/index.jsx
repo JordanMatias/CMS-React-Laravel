6ec60afb7beeaab31fddcbd970968ef2075a2ea3
Jordan <121629654+JordanMatias@users.noreply.github.com>
2023-02-25 08:17:34 -0500

    update some UI

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -165,6 +165,74 @@ export default function Layout({ view, heading }) {
                         ))}
                       </nav>
                     </div>
+<div className='flex-1 h-0 pt-5 pb-4 overflow-y-auto'>
+                      <div className='flex-shrink-0 flex items-center px-4'>
+                        <img
+                          className='h-8 w-auto'
+                          src='https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg'
+                          alt='Workflow'
+                        />
+                      </div>
+                      <nav className='mt-5 px-2 space-y-1'>
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? 'bg-gray-900 text-white'
+                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                              'group flex items-center px-2 py-2 text-base font-medium rounded-md'
+                            )}
+                          >
+                                       <div className='flex-1 h-0 pt-5 pb-4 overflow-y-auto'>
+                      <div className='flex-shrink-0 flex items-center px-4'>
+                        <img
+                          className='h-8 w-auto'
+                          src='https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg'
+                          alt='Workflow'
+                        />
+                      </div>
+                      <nav className='mt-5 px-2 space-y-1'>
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? 'bg-gray-900 text-white'
+                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                              'group flex items-center px-2 py-2 text-base font-medium rounded-md'
+                            )}
+                          >
+                            <item.icon
+                              className={classNames(
+                                item.current
+                                  ? 'text-gray-300'
+                                  : 'text-gray-400 group-hover:text-gray-300',
+                                'mr-4 flex-shrink-0 h-6 w-6'
+                              )}
+                              aria-hidden='true'
+                            />
+                            {item.name}
+                          </a>
+                        ))}
+                      </nav>
+                    </div>
+                            <item.icon
+                              className={classNames(
+                                item.current
+                                  ? 'text-gray-300'
+                                  : 'text-gray-400 group-hover:text-gray-300',
+                                'mr-4 flex-shrink-0 h-6 w-6'
+                              )}
+                              aria-hidden='true'
+                            />
+                            {item.name}
+                          </a>
+                        ))}
+                      </nav>
+                    </div>
                     <div className='flex-shrink-0 flex bg-gray-700 p-4'>
                       <a href='#' className='flex-shrink-0 group block'>
                         <div className='flex items-center'>
